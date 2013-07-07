eb8ab5503db670359f51c08c4e8d2db6e02516ad
XiaoxuShen <shenxiaoxu22@gmail.com>
2013-07-07 13:08:11 -0400

    Maximize sum of subset
    Maximize sum of subset such that if you include the number in sum, you
    may not use adjucent numbers to count in sum

---
+++ src/javaapplication20/JavaApplication20.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication20;
+
+/**
+ *
+ * @author XiaoxuShen
+ */
+public class JavaApplication20 {
+
+    /**
+     * @param args the command line arguments
+     * suppose you are given an array of int. for example A[ ] = {1, - 1, 3, 8 ,4 } 
+     * maximize the sum of subset such that if you include the number in sum, you may not use adjutant numbers to count in sum. 
+     * so in the example above the max sum is 1+ 8 = 9; 
+       in A[ ] = { 1, 5, 3, 9, 4 } the max sum is 5 + 9 = 14. 
+        and in A[ ] = { 1,2,3,4, 5} max sum i s 1 + 3 + 5 = 9
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
